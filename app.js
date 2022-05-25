bda9dd093e1dda260e8bdfcd371f0267db453d45
dlfud <99ysl@naver.com>
2022-05-25 11:36:40 +0900

    db에 좋아요 싫어요 반영

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host: "localhost",
     user: "sbsst", // 사용자이름
     password: "sbs123414", // 비번
-    database: "a6", // 데이터 베이스
+    database: "phrase", // 데이터 베이스
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
@@ -18,20 +18,54 @@ const port = 3000
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://cdpn.io",
+  origin: "https://cdpn.io",
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
 app.use(cors());
 
+/* 명언 랜덤 출력 */
 app.get('/phrase', async (req, res) => {
-  const[rows] = await pool.query(
-      `SELECT * FROM phrase ORDER BY RAND() LIMIT 1;`
-  );
-  res.json(rows);
+    
+    const[[row]] = await pool.query(
+        `SELECT * FROM phrase ORDER BY RAND() LIMIT 1`
+    );
+
+    await pool.query(
+        `UPDATE phrase SET hit = hit + 1 WHERE id = ?`,
+        [row.id]
+    );
+    res.json([row]);
+});
+
+app.post('/phrase', async (req, res) => {
+  const{likePoint, dislikePoint} = req.body;
+
+  if(!likePoint){
+    res.status(400).json({
+      msg:"like required",
+    })
+    return;
+  }
+
+  if(!dislikePoint){
+    res.status(400).json({
+      msg:"like required",
+    })
+    return;
+  }
+
+  await pool.query(
+    `UPDATE phrase SET likePoint = likePoint + 1, dislikePoint = dislikePoint + 1`
+  )
+
+  res.status(201).json({
+    msg:"성공",
+  });
 });
 
 app.listen(port);
+
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
